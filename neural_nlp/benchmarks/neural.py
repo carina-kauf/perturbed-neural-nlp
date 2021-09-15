c5ccd38f3ed8873b9fdb6d287ff02b886969ddac
Carina Kauf <ckauf@mit.edu>
2021-09-15 12:55:46 -0400

    rn backward bm

--- neural_nlp/benchmarks/neural.py
+++ neural_nlp/benchmarks/neural.py
@@ -772,19 +772,19 @@ class PereiraEncodingScrLowPMI(_PereiraBenchmarkScrambled):
     def ceiling(self):
         return super(PereiraEncodingScrLowPMI, self).ceiling
 
-class PereiraEncodingScrBackwards(_PereiraBenchmarkScrambled):
+class PereiraEncodingScrBackwardSent(_PereiraBenchmarkScrambled):
 
     def __init__(self, scrambled_version="backward", **kwargs):
         metric = CrossRegressedCorrelation(
             regression=linear_regression(xarray_kwargs=dict(stimulus_coord='stimulus_id')),
             correlation=pearsonr_correlation(xarray_kwargs=dict(correlation_coord='stimulus_id')),
             crossvalidation_kwargs=dict(splits=5, kfold=True, split_coord='stimulus_id', stratification_coord=None))
-        super(PereiraEncodingScrBackwards, self).__init__(metric=metric, scrambled_version=scrambled_version, **kwargs) # identifier='Pereira2018-encoding-scrambled-backward'
+        super(PereiraEncodingScrBackwardSent, self).__init__(metric=metric, scrambled_version=scrambled_version, **kwargs) # identifier='Pereira2018-encoding-scrambled-backward'
 
     @property
     @load_s3(key='Pereira2018-encoding-ceiling')
     def ceiling(self):
-        return super(PereiraEncodingScrBackwards, self).ceiling
+        return super(PereiraEncodingScrBackwardSent, self).ceiling
 
 class PereiraEncodingScrRandomWordlist(_PereiraBenchmarkScrambled):
 
@@ -1002,7 +1002,7 @@ benchmark_pool = [
     ('Pereira2018-encoding-scrambled7', PereiraEncodingScr7),
     ('Pereira2018-encoding-scrambled-lowpmi', PereiraEncodingScrLowPMI),
     #('Pereira2018-encoding-scrambled-random-lowPMI', PereiraEncodingScrRandomLowPMI), #lowPMI random word shuffling within sentence
-    ('Pereira2018-encoding-scrambled-backward', PereiraEncodingScrBackwards),
+    ('Pereira2018-encoding-scrambled-backward', PereiraEncodingScrBackwardSent),
     ('Pereira2018-encoding-scrambled-random-wl', PereiraEncodingScrRandomWordlist),
     #perturb benchmarks
     ('Pereira2018-encoding-perturb-nouns', PereiraEncodingPerturbedN), #keep only nouns
