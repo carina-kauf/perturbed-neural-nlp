9b13fb180fcae71f8980707e514a8f104736aa07
carina-kauf <ckauf@mit.edu>
2023-05-08 16:54:08 -0400

    restructure repo

--- analysis/gpt2_surprisals/get_surprisals.py
+++ analysis/gpt2_surprisals/get_surprisals.py
@@ -11,23 +11,20 @@ import torch
 import math
             
 
-def score(model, tokenizer, sentence, average=None):
+def score(model, tokenizer, sentence):
     tokenize_input = tokenizer.tokenize(tokenizer.eos_token + sentence + tokenizer.eos_token)
     tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
-    loss = model(tensor_input, labels=tensor_input)[0] #average negative log-likelihood for each token is returned as the loss
-    if average == "None":
-        sent_score = loss.item() * len(tokenize_input) #sentence negative log-likelihood
-    elif average == "avg":
-        sent_score = loss.item() #average negative log-likelihood per token
-    elif average == "avg_sentlength":
-        sent_score = loss.item() * len(tokenize_input) / len(sentence.split())
-    return sent_score
+    loss = model(tensor_input, labels=tensor_input)[0]
+    
+    sent_score = loss.item() * len(tokenize_input)
+    sent_scoreAvgTokenScore = loss.item()
+    sent_scoreAvgWordScore = loss.item() * len(tokenize_input) / len(sentence.split())
+    return sent_score, sent_scoreAvgTokenScore, sent_scoreAvgWordScore
 
 
-
-def main(average=None):
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version', type=str, default='distilgpt2')
+    parser.add_argument('--version', type=str, default='gpt2-xl')
     args = parser.parse_args()
     
     assert "gpt2" in args.version, "Invalid version specified! Currently only implemented for GPT models"
@@ -35,7 +32,7 @@ def main(average=None):
     out_dir = f'files/'
     os.makedirs(out_dir, exist_ok=True)
 
-    print("Loading model")
+    print("Loading model", flush=True)
     # Load pre-trained model (weights)
     model = GPT2LMHeadModel.from_pretrained(args.version)
     model.eval()
@@ -44,26 +41,25 @@ def main(average=None):
     
 
     # define conditions
-    conditions = [
-    ('Original', 'original'),
-        #
-    ('Scr1', 'scrambled'),
-    ('Scr3', 'scrambled'),
-    ('Scr5', 'scrambled'),
-    ('Scr7', 'scrambled'),
-    ('backward', 'scrambled'),
-    ('lowPMI', 'scrambled'),
-    ('random', 'scrambled'),
-    ('nouns', 'perturbed'),
-    ('randomnouns', 'perturbed'),
-    ('nounsverbs', 'perturbed'),
-    ('nounsverbsadj', 'perturbed'),
-    ('contentwords', 'perturbed'),
-    ('functionwords', 'perturbed'),
-        #
-    ('sentenceshuffle-random', 'perturbed'),
-    ('sentenceshuffle-withinpassage', 'perturbed'),
-    ('sentenceshuffle-withintopic', 'perturbed')
+    conditions = [('Original', 'original'),
+                    ('Scr1', 'scrambled'),
+                    ('Scr3', 'scrambled'),
+                    ('Scr5', 'scrambled'),
+                    ('Scr7', 'scrambled'),
+                    ('backward', 'scrambled'),
+                    ('lowPMI', 'scrambled'),
+                    ('lowPMI_random', 'scrambled'),
+                    ('random', 'scrambled'),
+                    ('nouns', 'perturbed'),
+                    ('randomnouns', 'perturbed'),
+                    ('nounsverbs', 'perturbed'),
+                    ('nounsverbsadj', 'perturbed'),
+                    ('contentwords', 'perturbed'),
+                    ('functionwords', 'perturbed'),
+                        #
+                    ('sentenceshuffle-random', 'perturbed'),
+                    ('sentenceshuffle-withinpassage', 'perturbed'),
+                    ('sentenceshuffle-withintopic', 'perturbed')
     ]
 
     # Load pickles and get sentences for all conditions
@@ -85,34 +81,35 @@ def main(average=None):
 
     # Get scores per condition and write to file
     for key, sentences in COND2SENT.items():
-        out_name = f'{args.version}.{key}.sentence_surp.txt'
-        if average == "avg":
-            out_name = out_name.rstrip(".txt") + ".average.txt"
-        if average == "avg_sentlength":
-            out_name = out_name.rstrip(".txt") + ".average_sentlength.txt"
-        out_name = os.path.join(out_dir, out_name)
-        print(out_name)
+        out_name = os.path.join(f'{args.version}.{key}.sentence_surp.txt')
+        out_nameTokAverage = out_name.rstrip(".txt") + ".average.txt"
+        out_nameWordAverage = out_name.rstrip(".txt") + ".average_sentlength.txt"
 
-        print(f"Getting {args.version} probabilities for condition {key}")
+        print(f"Getting {args.version} probabilities for condition {key}", flush=True)
         scores = []
+        AvgTokenScores = []
+        AvgWordScores = []
+        
         for sent in tqdm(sentences):
-            sent_score = score(model, tokenizer, sent, average=average)
+            sent_score, sent_scoreAvgTokenScore, sent_scoreAvgWordScore = score(model, tokenizer, sent)
             scores.append(sent_score)
+            AvgTokenScores.append(sent_scoreAvgTokenScore)
+            AvgWordScores.append(sent_scoreAvgWordScore)
 
         with open(out_name, "w") as fout:
             fout.write(f'Index\tSentence\tSurprisal\tPPL\n') #add header
             for i, sent, sent_score in zip(range(len(sentences)), sentences, scores):
                 fout.write(f'{i}\t{sent}\t{sent_score}\t{math.exp(sent_score)}\n')
                 
+        with open(out_nameTokAverage, "w") as fout:
+            fout.write(f'Index\tSentence\tSurprisal\tPPL\n') #add header
+            for i, sent, sent_score in zip(range(len(sentences)), sentences, AvgTokenScores):
+                fout.write(f'{i}\t{sent}\t{sent_score}\t{math.exp(sent_score)}\n')
+                
+        with open(out_nameWordAverage, "w") as fout:
+            fout.write(f'Index\tSentence\tSurprisal\tPPL\n') #add header
+            for i, sent, sent_score in zip(range(len(sentences)), sentences, AvgWordScores):
+                fout.write(f'{i}\t{sent}\t{sent_score}\t{math.exp(sent_score)}\n')
+                
 if __name__ == "__main__":
-    for average_method in ["None", "avg", "avg_sentlength"]:
-        print(f"Running for average: {average_method}")
-        main(average=average_method)
-      
-    #TESTS FOR METHOD "AVG":
-    #lm-scorer check: https://colab.research.google.com/drive/1rsNHqpCMRwHGsHy4bQVeKSEt_VZQ_Rmm#scrollTo=oGgGp4axbZTR
-    #>> exactly the same scores
-    #minicons check: https://colab.research.google.com/drive/1Hdw3Jf2wlGgpwldfFElxesuiQVPQW3bZ#scrollTo=V1N3VzGy26AC
-    #>> almost exactly the same scores
-    #lm-zoo check: https://colab.research.google.com/drive/1LHaJ2LrQwi-ZLMlpAujZ0J-DqwZClunh#scrollTo=BSbBR5UIXFd3
-    #>> slightly different scores
+    main()
